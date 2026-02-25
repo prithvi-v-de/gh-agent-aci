ea0647b763bf229e39b770c1fc8bb823f569c9f4
prithvi-v-de <prithvi.vasireddy@dataeconomy.ai>
2026-02-25 12:54:52 -0500

    Update my_agent.py

--- my_agent.py
+++ my_agent.py
@@ -14,7 +14,8 @@ from bedrock_agentcore.identity.auth import requires_access_token
 @requires_access_token(
     provider_name="github-provider",
     scopes=["repo", "read:user"],
-    auth_flow='USER_FEDERATION' 
+    auth_flow='USER_FEDERATION',
+    return_url="https://gh-agent-aci-aq6nrmcvn96tycbvhnqjgf.streamlit.app/"
 )
 def _internal_github_profile(access_token=None):
     headers = {
