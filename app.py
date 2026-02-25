8f9c022f8386bad7ff181860f881daeb4ad3258c
prithvi-v-de <prithvi.vasireddy@dataeconomy.ai>
2026-02-25 15:22:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import boto3
 import uuid
 import json
 
-# Replace with your actual AgentCore ARN from the AWS Console
 AGENT_ARN = "arn:aws:bedrock-agentcore:us-east-1:819079555973:runtime/myagent-bHKPpuHli3"
 client = boto3.client('bedrock-agentcore', region_name="us-east-1")
 
@@ -12,7 +11,6 @@ st.title("Talk to my AWS Agent 🤖")
 
 
 
-# AUTOMATIC WARMUP
 if "warmed_up" not in st.session_state:
     with st.spinner("Waking up agent..."):
         try:
@@ -27,19 +25,16 @@ if "warmed_up" not in st.session_state:
             st.warning("Agent is still sleeping. First message might take a moment.")
 
 
-# Initialize Session state for Chat
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display persistent chat history
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# User input processing
 if prompt := st.chat_input("Ask me anything..."):
     with st.chat_message("user"):
         st.markdown(prompt)
